a2da83b9994ec7e901d19d2420afcdac3329093e
Sewanonda Keith Edwin <skeith696@gmail.com>
2022-01-31 21:57:07 +0300

    style: create quote card

---
+++ components/qoutecard.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card :loading="loading">
+        <template slot="progress">
+            <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+            ></v-progress-linear>
+        </template>
+      <v-card-text>
+        <v-chip-group
+          column
+        >
+          <v-chip
+            v-for="tag in tags"
+            :key="tag"
+          >
+            {{ tag }}
+          </v-chip>
+        </v-chip-group>
+      </v-card-text>
+        <v-card-title class="headline">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Sint quas aliquam delectus repellendus incidunt dolorum aperiam odio voluptatum nihil! 
+          Quasi consequatur dolore necessitatibus nihil inventore debitis dicta porro ipsum quod.
+        </v-card-title>
+        <v-card-subtitle>John doe</v-card-subtitle>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary"> Generate </v-btn>
+        </v-card-actions>
+      </v-card>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        loading: false,
+        tags: [
+            'Work',
+            'Home Improvement',
+            'Vacation',
+            'Food',
+            'Drawers',
+            'Shopping',
+            'Art',
+            'Tech',
+            'Creative Writing',
+        ],
+    }),
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,15 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-        </v-card-title>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary"> Generate </v-btn>
-        </v-card-actions>
-      </v-card>
+      <Qoutecard />
     </v-col>
   </v-row>
 </template>
 
 <script>
+import Qoutecard from "~/components/qoutecard.vue";
 export default {
-  name: 'IndexPage',
+    name: "IndexPage",
+    components: { Qoutecard }
 }
 </script>
